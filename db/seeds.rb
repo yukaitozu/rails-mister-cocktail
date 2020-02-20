c34b45be0f052899bae61895895458657c909f72
Yuka Itozu <yukaitozu@gmail.com>
2020-02-20 17:31:32 +0900

    mister cocktail complete without css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'JSON'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+json = open(url).read
+drinks = JSON.parse(json)
+drinks['drinks'].each do |obj|
+  Ingredient.create!(name: obj['strIngredient1'])
+end
